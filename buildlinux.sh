701e50015e37564e4e4caa6108fe35caf18d11d4
MarceloMaurin <marcelomaurinmartins@gmail.com>
2021-02-15 12:13:00 -0300

    atualização linux

--- buildlinux.sh
+++ buildlinux.sh
@@ -2,5 +2,5 @@
 cp ./src/MNote2 ./mnote2/usr/bin/
 dpkg-deb --build mnote2
 mv mnote2.deb mnote2-2.8_amd64.deb
-cp mnote2-2.8_amd64.deb ../bin/mnote2-2.8_amd64.deb
+cp ./mnote2-2.8_amd64.deb ./bin/
 
