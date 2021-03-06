5fa09dd911bccdf41de308483cef983bd6061325
MarceloMaurin <marcelomaurinmartins@gmail.com>
2021-03-06 12:36:26 -0300

    ok

--- buildlinux.sh
+++ buildlinux.sh
@@ -26,7 +26,7 @@ then
 	exit 1;
 fi
 
-if [ $ARQUITETURA = 'i686' ]; 
+if [ $ARQUITETURA = 'i686' ];
 then
 	echo "i686 Script"
 	echo "Preparando binarios"
@@ -40,6 +40,23 @@ then
 	echo "Movendo para pasta repositorio"
 	mv mnote2.deb mnote2-2.9_i686.deb
 	cp ./mnote2-2.9_i686.deb ./bin/
+	exit 1;
+fi
+
+if [ $ARQUITETURA = 'i386' ];
+then
+	echo "i386 Script"
+	echo "Preparando binarios"
+	cp ./src/MNote2 ./mnote2/usr/bin/mnote2
+	chmod 777 ./mnote2/usr/bin/mnote2
+	cp ./src/MNote2.png ./mnote2/usr/share/icons/hicolor/mnote2.png
+	cp ./mnote2.desktop_arm ./mnote2/usr/share/applications/mnote2.desktop
+	#ln -s /usr/bin/MNote2 ./mnote2/usr/share/applications/mnote2
+	echo "Empacotando"
+	dpkg-deb --build mnote2
+	echo "Movendo para pasta repositorio"
+	mv mnote2.deb mnote2-2.9_i386.deb
+	cp ./mnote2-2.9_i386.deb ./bin/
 	exit 1;
 fi
 
