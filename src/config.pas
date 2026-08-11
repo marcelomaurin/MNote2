917e2deb958c93b0ed747e97976b68dd0ec1208c
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-08-11 09:51:15 -0300

    validate general configuration screen

--- src/config.pas
+++ src/config.pas
@@ -44,9 +44,8 @@ type
     procedure btSaveClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
-
+    function ValidateControls(out AError: string): Boolean;
   public
-
   end;
 
 var
@@ -58,21 +57,40 @@ implementation
 
 { Tfrmconfig }
 
-procedure Tfrmconfig.btSaveClick(Sender: TObject);
+function Tfrmconfig.ValidateControls(out AError: string): Boolean;
 begin
-  FSetMain.Compile := edCompile.Text;
-  FSetMain.Install := edInstall.Text;
-  FSetMain.CleanScript := edClean.Text;
-  FSetMain.RunScript := edRun.Text;
-  FSetMain.DebugScript := edDebug.Text;
-  FSetMain.DLLMYPath := edDLLMYPATH.Text;
-  FSetMain.DLLPOSTPath := edDLLPOSTPATH.Text;
-  FSetMain.DLLMSSQLPath := edDLLMSSQLPATH.Text;
-  FSetMain.DLLOraclePath := edDLLOraclePATH.Text;
-  FSetMain.ToolsOuvir := ckToolsOuvir.Checked;
-  FSetMain.IPOUVIR := edIPOUVIR.Text;
+  AError := '';
+  if ckToolsOuvir.Checked and (Trim(edIPOuvir.Text) = '') then
+  begin
+    AError := 'Informe o IP do reconhecimento de voz ou desative a opção.';
+    Exit(False);
+  end;
+  Result := True;
+end;
 
+procedure Tfrmconfig.btSaveClick(Sender: TObject);
+var
+  ErrorText: string;
+begin
+  if not ValidateControls(ErrorText) then
+  begin
+    MessageDlg('Configuração', ErrorText, mtWarning, [mbOK], 0);
+    Exit;
+  end;
+
+  FSetMain.Compile := Trim(edCompile.Text);
+  FSetMain.Install := Trim(edInstall.Text);
+  FSetMain.CleanScript := Trim(edClean.Text);
+  FSetMain.RunScript := Trim(edRun.Text);
+  FSetMain.DebugScript := Trim(edDebug.Text);
+  FSetMain.DLLMYPath := Trim(edDLLMYPATH.Text);
+  FSetMain.DLLPOSTPath := Trim(edDLLPOSTPATH.Text);
+  FSetMain.DLLMSSQLPath := Trim(edDLLMSSQLPATH.Text);
+  FSetMain.DLLOraclePath := Trim(edDLLOraclePATH.Text);
+  FSetMain.ToolsOuvir := ckToolsOuvir.Checked;
+  FSetMain.IPOUVIR := Trim(edIPOUVIR.Text);
   FSetMain.SalvaContexto(False);
+  ModalResult := mrOK;
   Close;
 end;
 
@@ -93,6 +111,7 @@ end;
 
 procedure Tfrmconfig.btCancelClick(Sender: TObject);
 begin
+  ModalResult := mrCancel;
   Close;
 end;
 
